61903f02da3d8091f9bfc0512d9fd792e104a2a1
unknown <get2henry14@gmail.com>
2026-05-24 10:46:17 -0700

    created an app sub-directory with two files inside  the layout and page file.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AuthPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Welcome to worknoon-chat-frontend</h1>
+    </div>
+  );
+}
